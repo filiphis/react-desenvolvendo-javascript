0875649f92e949da1cafed095367b0564b17d7cb
Luiz Felipe da Silveira <l.filiphis@hotmail.com>
2023-03-26 23:20:58 -0300

    :lipstick: Adjusting single key errors and final adjustments

--- README.md
+++ README.md
@@ -3,9 +3,9 @@
 
 # React desenvolvendo com javascript
 
-<!-- > **_Status:_** Finalizado :heavy_check_mark: -->
+> **_Status:_** Finalizado :heavy_check_mark:
 
-> **_Status:_** Em andamento :construction:
+<!-- > **_Status:_** Em andamento :construction: -->
 
 ## Descrição
 
--- src/components/Colaborador/index.jsx
+++ src/components/Colaborador/index.jsx
@@ -1,10 +1,15 @@
 import "./styles.css";
 
 const Colaborador = ({ imagem, nome, cargo }) => {
+  const imageNotLoad = (event) => {
+    const defaultImage =
+      "https://cdn-icons-png.flaticon.com/512/149/149071.png";
+    event.target.src = defaultImage;
+  };
   return (
     <div className="colaborador">
       <div className="cabecalho">
-        <img src={imagem} alt={nome} />
+        <img src={imagem} alt={nome} onError={imageNotLoad} />
       </div>
       <div className="rodape">
         <h4>{nome}</h4>
